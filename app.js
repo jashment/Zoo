1a38aadfc4369484600eef381a8266fe986113ad
jashment <jordanashment@gmail.com>
2019-02-25 16:29:42 -0700

    Basically starting over...again

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const animal = require('./routes/animal.route')
+const app = express()
+
+const mongoose = require('mongoose')
+
+let dev_db_url = 'mongodb+srv://jashment:Bowzerdog1!@cluster0-dh4uw.mongodb.net/test?retryWrites=true'
+let mongoDB = process.env.MONGODB_URI || dev_db_url
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use('/animals', animal)
 
 let port = 5000
 
